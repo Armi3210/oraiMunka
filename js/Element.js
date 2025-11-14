fd421f608dc7d649c01ac99cc54cfdb27e8c14c4
Armi3210 <125443940+Armi3210@users.noreply.github.com>
2025-11-14 11:35:23 +0100

    Add files via upload

--- js/Element.js
+++ js/Element.js
@@ -18,5 +18,40 @@ class Element{
         this.#y =y;
     }
 
+    setContainer(container){
+        this.#container = document.querySelector(container);
+    }
 
+    createelement(){
+        this.#element = document.createElement(this.#container);
+        this.#element.style.left = this.#x + "px";
+        this.#element.style.top = this.#y + "px";
+
+        const r = Math.random()*5;
+
+        if (r>3){
+            this.#isMole = 2;
+        }
+        else{
+            this.#isMole = parseInt(Math.random()*2)
+        }
+
+        if (this.#isMole === 0){
+            this.#element.classList.add("mole")
+        } else if (this.#isMole === 1){
+            this.#element.classList.add("bomb")
+        }
+
+        this.#container.appendChild(this.#element);
+        this.#element.addEventListener('click', () => this.collect());
+    }
+
+    remove(){
+        this.#element.remove();
+    };
+
+    collect(){
+        thios.remove();
+        updateScore((this.#isMole))
+    }
 }
