5a0a4074a9bf7ef9faa1705dfcaedb24c7c87abb
Armi3210 <125443940+Armi3210@users.noreply.github.com>
2025-11-14 11:07:08 +0100

    Alapok

---
+++ js/Element.js
@@ -0,0 +1,22 @@
+class Element{
+    #x;
+    #y;
+    #container;
+    #isMole;
+    #element;
+    #interval;
+
+    constructor(x,y,container){
+
+    }
+
+    setX(x) {
+        this.#x =x;
+    }
+
+    setY(y) {
+        this.#y =y;
+    }
+
+
+}
